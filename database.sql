61ae11c28f168e0b57719134d4f1e0a4aec8289b
Voon <voonch85@gmail.com>
2025-07-04 23:38:06 +0800

    Add contacts table

--- database.sql
+++ database.sql
@@ -21,3 +21,14 @@ CREATE TABLE users_profiles (
     location VARCHAR(100),
     FOREIGN KEY (account_id) REFERENCES accounts(account_id)
 );
+
+CREATE TABLE contacts (
+    contact_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    category ENUM('bug', 'feedback', 'suggestion', 'other'),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    message TEXT NOT NULL,
+    submitted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users_profiles(user_id)
+);
